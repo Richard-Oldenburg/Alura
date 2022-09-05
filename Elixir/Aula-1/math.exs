2d2bc77abaec2d98d0552ac13a8e0227b4e84185
Richard Oldenburg <richard.oldenburg@live.com>
2022-09-05 17:06:25 -0300

    Iniciando em Elixir

---
+++ Elixir/Aula-1/math.exs
@@ -0,0 +1,9 @@
+# defmodule MeuModulo.Math do
+#   def soma(parametro1, parametro2) do
+#     parametro1 + parametro2
+#   end
+# end
+
+defmodule MeuModulo.Math do
+  def soma(parametro1, parametro2), do: parametro1 + parametro2
+end
