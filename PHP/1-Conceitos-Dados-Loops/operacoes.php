901c28b1df0afd43d79cfd60449ff02e0e98c4e2
Richard Oldenburg <richard.oldenburg@live.com>
2022-09-19 17:14:02 -0300

    PHP Curso 1

---
+++ PHP/1-Conceitos-Dados-Loops/operacoes.php
@@ -0,0 +1,18 @@
+<?php
+
+$idade = 21;
+$idadeDaqui10Anos = $idade + 10;
+
+$soma = 2 + 2;
+$subtracao = 2 - 2;
+$multiplicacao = 2 * 2;
+$divisao = 2 / 2;
+
+$doisAoCubo = 2 ** 3;
+//ou
+$doisAoCubo = 2 * 2 * 2;
+
+$restoDaDivisao = 10 % 3;
+
+
+echo $idadeDaqui10Anos;
